ab5bba0de88d209fffe5ca34902843f74132657d
NotPeOpLe <code840@outlook.com>
2023-03-09 14:56:20 +0800

    :new: 新增bullshit生產器頁面

--- app.vue
+++ app.vue
@@ -1,70 +1,23 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center bg-slate-900">
-    <img src="~/assets/skaibu.png" alt="skaibu">
-    <h1 class="text-3xl font-bold text-white">
-      SKaiBlue
-    </h1>
-    <div class="flex space-x-2 text-white mt-3">
-      <a class="bg-purple-700 rounded-lg px-2 py-1" href="https://www.twitch.tv/skaibu" target="_blank">
-        <div class="flex pointer-events-none">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitch mr-1" width="24"
-            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-            stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path
-              d="M4 5v11a1 1 0 0 0 1 1h2v4l4 -4h5.584c.266 0 .52 -.105 .707 -.293l2.415 -2.414c.187 -.188 .293 -.442 .293 -.708v-8.585a1 1 0 0 0 -1 -1h-14a1 1 0 0 0 -1 1z">
-            </path>
-            <line x1="16" y1="8" x2="16" y2="12"></line>
-            <line x1="12" y1="8" x2="12" y2="12"></line>
-          </svg>
-          Twitch
-        </div>
-      </a>
-
-      <a class="flex bg-blue-500 rounded-lg px-2 py-1" href="https://twitter.com/SKaiBlue__" target="_blank">
-        <div class="flex pointer-events-none">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter mr-1" width="24"
-            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-            stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path
-              d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z">
-            </path>
-          </svg>
-          Twitter
-        </div>
-      </a>
-
-      <a class="flex bg-pink-500 rounded-lg px-2 py-1" href="https://osu.ppy.sh/users/579370" target="_blank">
-        <div class="flex pointer-events-none">
-          <svg width="300" class="mr-1 w-6 h-6" height="300" version="1.1" viewBox="0 0 300 300" stroke="currentColor"
-            xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
-            <path id="osu_x21__x5F_txt_x5F_Path_2_"
-              d="m75.1 181.4c-4.7 0-8.8-0.8-12.3-2.3s-6.4-3.7-8.6-6.4c-2.3-2.7-4-5.9-5.2-9.6s-1.7-7.6-1.7-11.9 0.6-8.3 1.7-12c1.2-3.7 2.9-7 5.2-9.7s5.2-4.9 8.6-6.5 7.6-2.4 12.3-2.4 8.8 0.8 12.3 2.4 6.4 3.7 8.8 6.5c2.3 2.7 4 6 5.2 9.7 1.1 3.7 1.7 7.7 1.7 12s-0.6 8.2-1.7 11.9-2.8 6.9-5.2 9.6c-2.3 2.7-5.2 4.9-8.8 6.4-3.4 1.6-7.6 2.3-12.3 2.3zm0-12.1c4.2 0 7.2-1.6 9-4.7s2.7-7.6 2.7-13.4-0.9-10.3-2.7-13.4-4.8-4.7-9-4.7c-4.1 0-7.1 1.6-8.9 4.7s-2.7 7.6-2.7 13.4 0.9 10.3 2.7 13.4c1.8 3.2 4.8 4.7 8.9 4.7zm51.8-14.5c-4.2-1.2-7.5-3-9.8-5.3-2.4-2.4-3.5-5.9-3.5-10.6 0-5.7 2-10.1 6.1-13.4 4.1-3.2 9.6-4.8 16.7-4.8 2.9 0 5.8 0.3 8.6 0.8s5.7 1.3 8.6 2.4c-0.2 1.9-0.5 4-1.1 6.1s-1.3 3.9-2.1 5.5c-1.8-0.7-3.8-1.4-5.9-2-2.2-0.6-4.5-0.8-6.8-0.8-2.5 0-4.5 0.4-5.9 1.2s-2.1 2-2.1 3.8c0 1.6 0.5 2.8 1.5 3.5s2.4 1.3 4.3 1.9l6.4 1.9c2.1 0.6 4 1.3 5.7 2.2s3.1 1.9 4.3 3.2 2.1 2.8 2.8 4.7 1 4.2 1 6.8c0 2.8-0.6 5.3-1.7 7.7-1.2 2.4-2.8 4.5-5 6.2-2.2 1.8-4.9 3.1-8 4.2-3.1 1-6.7 1.5-10.7 1.5-1.8 0-3.4-0.1-4.9-0.2s-2.9-0.3-4.3-0.6-2.7-0.6-4.1-1c-1.3-0.4-2.8-0.9-4.4-1.5 0.1-2 0.5-4.1 1.1-6.1 0.6-2.1 1.3-4.1 2.2-6 2.5 1 4.8 1.7 7 2.2s4.5 0.7 6.9 0.7c1 0 2.2-0.1 3.4-0.3s2.4-0.5 3.4-1 1.9-1.1 2.6-1.9 1.1-1.8 1.1-3.1c0-1.8-0.5-3.1-1.6-3.9s-2.6-1.5-4.5-2.1zm39.3-32.7c2.7-0.4 5.3-0.7 8-0.7 2.6 0 5.3 0.2 8 0.7v30.7c0 3.1 0.2 5.6 0.7 7.6s1.2 3.6 2.2 4.7c1 1.2 2.3 2 3.8 2.5s3.3 0.7 5.3 0.7c2.8 0 5.1-0.3 7-0.8v-45.4c2.7-0.4 5.3-0.7 7.9-0.7s5.3 0.2 8 0.7v55.8c-2.4 0.8-5.6 1.6-9.5 2.4s-8 1.2-12.3 1.2c-3.8 0-7.5-0.3-11-0.9s-6.6-1.9-9.3-3.8-4.8-4.8-6.3-8.5c-1.6-3.7-2.4-8.7-2.4-14.9v-31.3zm65.9 58c-0.4-2.8-0.7-5.5-0.7-8.2s0.2-5.5 0.7-8.3c2.8-0.4 5.5-0.7 8.2-0.7s5.5 0.2 8.3 0.7c0.4 2.8 0.7 5.6 0.7 8.2 0 2.8-0.2 5.5-0.7 8.3-2.8 0.4-5.6 0.7-8.2 0.7-2.8-0.1-5.5-0.3-8.3-0.7zm-0.4-80.7c2.9-0.4 5.8-0.7 8.6-0.7 2.9 0 5.8 0.2 8.8 0.7l-1.1 54.9c-2.6 0.4-5.1 0.7-7.5 0.7-2.5 0-5.1-0.2-7.6-0.7z"
-              fill="currentColor" />
-            <path id="Rim_6_"
-              d="m150 0c-82.8 0-150 67.2-150 150s67.2 150 150 150 150-67.2 150-150-67.2-150-150-150zm0 285c-74.6 0-135-60.4-135-135s60.4-135 135-135 135 60.4 135 135-60.4 135-135 135z"
-              fill="currentColor" />
-          </svg>
-          Osu!
-        </div>
-      </a>
-    </div>
-  </div>
+  <NuxtPage />
 </template>
 
 <script lang="ts" setup>
 const bullshit = useBullshit()
 
 useHead({
-  title: 'SKaiBlue',
+  title: "SKaiBlue",
   meta: [
     { property: "og:type", content: "website" },
     { property: "og:title", content: "SKaiBlue" },
     { property: "og:description", content: bullshit.generate("凱哥", 200) },
     { property: "og:url", content: "https://skaibu.gay/" },
-    { property: "og:image", content: "~/assets/skaibu.png" },
+    {
+      property: "og:image",
+      content:
+        "https://cdn.discordapp.com/attachments/912960977249660978/1066005200961286314/skaibu.png",
+    },
     { name: "theme-color", content: "#00D6FF" },
-  ]
+  ],
 })
 </script>
---
+++ pages/bullshit.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="h-screen flex flex-col justify-center items-center bg-slate-900">
+    <img
+      src="https://cdn.discordapp.com/attachments/912960977249660978/1066005200961286314/skaibu.png"
+      alt="skaibu"
+    />
+    <NuxtLink to="/" class="text-3xl font-bold text-white">SKaiBlue</NuxtLink>
+    <div class="mx-20 my-5 text-white">
+      {{ result }}
+    </div>
+    <div>
+      <button
+        class="transition bg-blue-500 text-white rounded-lg px-2 py-1 hover:bg-blue-600 active:bg-blue-700 select-none"
+        @click="refresh"
+      >
+        刷新
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: "SKaiBlue",
+  meta: [
+    { property: "og:type", content: "website" },
+    { property: "og:title", content: "SKaiBlue" },
+    { property: "og:description", content: "關於凱哥的幹話生產器" },
+    { property: "og:url", content: "https://skaibu.gay/" },
+    {
+      property: "og:image",
+      content:
+        "https://cdn.discordapp.com/attachments/912960977249660978/1066005200961286314/skaibu.png",
+    },
+    { name: "theme-color", content: "#00D6FF" },
+  ],
+})
+
+const bullshit = useBullshit()
+const result = ref("等我一下...")
+
+function refresh() {
+  result.value = bullshit.generate("凱哥", 30)
+}
+
+onMounted(() => refresh())
+</script>
